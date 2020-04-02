eec870d14ab9db344f293e86d6195f1cfc3b11ee
MuhBayu <bnugraha00@gmail.com>
2020-04-03 01:18:06 +0700

    shortcode

--- service/insta.ts
+++ service/insta.ts
@@ -1,11 +1,26 @@
 import unirest from 'unirest'
 
+function validURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(str);
+  }
+
 export const getMedia = (url) => {
-    let url_split= url.replace(/\/$/i, '').split('/')
-    if(url_split && url_split.length > 4) {
-        var shortcode = url_split[4]
+    let shortcode
+    if(validURL(url)) {
+        let url_split= url.replace(/\/$/i, '').split('/')
+        if(url_split && url_split.length > 4) {
+            shortcode = url_split[4]
+        } else {
+            return null
+        }
     } else {
-        return null
+        shortcode = url
     }
     return unirest.get(`https://www.instagram.com/p/${shortcode}/?__a=1`).then(data => {
         try {
